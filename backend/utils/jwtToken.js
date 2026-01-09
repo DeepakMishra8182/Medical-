72bd10f6e82c94e1d8a721307bc640c8c1d7125d
DeepakMishra8182 <hithub8182@gmail.com>
2026-01-09 18:14:10 +0530

    changed

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,15 +1,24 @@
 export const generateToken = (user, message, statusCode, res) => {
   const token = user.generateJsonWebToken();
-  // Determine the cookie name based on the user's role
-  const cookieName = user.role === 'Admin' ? 'adminToken' : 'patientToken';
+
+  // cookie name based on role
+  const cookieName =
+    user.role === "Admin"
+      ? "adminToken"
+      : user.role === "Patient"
+      ? "patientToken"
+      : "doctorToken";
 
   res
     .status(statusCode)
     .cookie(cookieName, token, {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-      ),
       httpOnly: true,
+      secure: true,        
+      sameSite: "None",    
+      expires: new Date(
+        Date.now() +
+          Number(process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000
+      ),
     })
     .json({
       success: true,
@@ -18,4 +27,3 @@ export const generateToken = (user, message, statusCode, res) => {
       token,
     });
 };
-
